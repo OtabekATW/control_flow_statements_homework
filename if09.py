94438b7dc6b512189c67cb2802a18bb494d63a76
OtabekATW <otabektursunov468@gmail.com>
2022-11-22 17:05:59 +0500

    if09.py

--- if09.py
+++ if09.py
@@ -2,11 +2,21 @@ def main(a):
     """
     The two-digit integer is given.
     Replace the digits of the number.
-    True if the resulting number is less than or equal to the old number, otherwise return False.
+    True if the resulting number is less than or equal to the old number,    return False.
     
     Args:
         a: integer
     Returns:
         boolean: True if the resulting number is less than or equal to the old number, otherwise return False.
     """
-    return
+
+    x1 = a % 10
+    x2 = a // 10
+    y = x1 * 10 + x2
+
+    if y <= a:
+        s = True 
+    if y >= a:
+        s = False
+
+    return s
